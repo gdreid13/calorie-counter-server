78846c7efb66d26a56e0949bc51c4994e644b316
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-09-23 14:59:52 -0700

    update meal table

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+const { DATABASE_URL } = require('./src/config')
 
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
       ? process.env.TEST_DATABASE_URL
-      :'postgres://wrpxnebfyculaj:f5229e3f2f950c2bf786b9e3786b9d062e543a929c102d94de5056d4023093e8@ec2-107-22-33-173.compute-1.amazonaws.com:5432/dcu742murmuh29',
+      : DATABASE_URL,
     "ssl": true,
   }
+/*
+'postgres://wrpxnebfyculaj:f5229e3f2f950c2bf786b9e3786b9d062e543a929c102d94de5056d4023093e8@ec2-107-22-33-173.compute-1.amazonaws.com:5432/dcu742murmuh29'
+ */
