6c6cfd95f2f7d42e8ffab1842b046bc52f1d9fce
gdreid13 <gdreid13@gmail.com>
2020-09-22 17:17:39 -0700

    re-adding weird migrate:production script

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL:process.env.DATABASE_URL,
-  JWT_SECRET:"secret",
-  JWT_EXPIRY:'24h',
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: "secret",
+  JWT_EXPIRY: '24h',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   API_TOKEN: process.env.API_TOKEN,
 }
