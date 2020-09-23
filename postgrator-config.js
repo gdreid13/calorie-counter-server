35efe41ac02049baeeea2e09d4afb5571dab5ffa
tyonek <tyonek.thompson@gmail.com>
2020-09-23 00:26:18 -0400

    patch

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,6 +5,6 @@ module.exports = {
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
       ? process.env.TEST_DATABASE_URL
-      : 'postgres://lvrlcwxwmvnifd:52091952405b4355767c76f6e06cc3edb2fd3318e0c7eec336f1070650c1c8c9@ec2-107-22-33-173.compute-1.amazonaws.com:5432/dbh783irngqn00',
+      : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
   }
