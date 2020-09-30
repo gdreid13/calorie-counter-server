54e19cf277a7d207bf4847599ff98fad128157c8
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-09-29 19:23:02 -0700

    update files

--- src/meals/meals-router.js
+++ src/meals/meals-router.js
@@ -29,54 +29,37 @@ mealsRouter.route('/')
           res.status(200).json(sanitizedMeal)
         }).catch(next)
 
-    /*
-mealsRouter
-  .route('/')
-  .get(requireAuth, (req, res, next) => {
-    MealsService.getAllMeals(req.app.get('db'))
-      .then(meal => {
-        res.json(meal.map(MealsService.serializeMeals))
-      })
-      .catch(next)*/
   })
   .post(jsonBodyParser, (req, res, next) => {
-    console.log(req.body)
-    const newMeal = req.body
-/*     for (const [key, value] of Object.entries(newMeal))
+    const {userid, dateofmeal,breakfast_food,breakfast_calories,lunch_food,lunch_calories,dinner_food,dinner_calories,alldaycalories} = req.body
+
+    const newMeal= {userid, dateofmeal,breakfast_food,breakfast_calories,lunch_food,lunch_calories,dinner_food,dinner_calories,alldaycalories}
+  /*   
+    for (const [key, value] of Object.entries(newMeal))
       if (value == null)
         return res.status(400).json({
           error: `Missing '${key}' in request body`
-      }) */
+      }) 
+  */
 
-    // commented out validation above because we want the user
-    // to be able to post breakfast, lunch, or dinner without having
-    // the other fields filled out.  can change this later
+    // commented out validation above because breakfast/lunch/dinner are not required, can change this later
 
-    // const sanitizedMeal= sanitizeItem(newMeal)
+    const sanitizedMeal= sanitizeItem(newMeal)
     
-/*     GeneralService.insertItem(req.app.get('db'),'meals',sanitizedMeal)
+    GeneralService.insertItem(req.app.get('db'),'meals',sanitizedMeal)
       .then(meal=>{
         res.status(201)
         .location(path.posix.join(req.originalUrl,`/${meal.id}`))
         .json(meal)
       })
-      .catch(next) */
+      .catch(next) 
+
     // trying to isolate issue, trying with simpler code below
-
-    // newMeal.author_id = req.user.id
-    // not sure of the purpose of this line
-
-    console.log(newMeal)
-    MealsService.insertMeals(
-      req.app.get('db'),
-      newMeal
-    )
-      .then(meal => {
-        res
-          .status(201)
-          .json(meal)
-      })
+    /*
+    MealsService.insertMeals( req.app.get('db'),newMeal)
+      .then(meal => res.status(201).json(meal))
       .catch(next)
+    */
   })
   
 
@@ -91,23 +74,20 @@ mealsRouter.route('/:id')
       GeneralService.deleteItem(req.app.get('db'),'meals',req.params.id)
         .then(()=>res.status(200).json('Meal has been deleted'))
   })
-.patch(jsonBodyParser,(req,res,next)=>{
-      const {userId, alldaycalories}= req.body
-      const mealToUpdate={userId,alldaycalories}
+  .patch(jsonBodyParser,(req,res,next)=>{
+      const {alldaycalories,breakfast_calories,breakfast_food,lunch_food,lunch_calories,dinner_food,dinner_calories}= req.body
+      const mealToUpdate={alldaycalories,breakfast_calories,breakfast_food,lunch_food,lunch_calories,dinner_food,dinner_calories}
       return GeneralService.updateItem(req.app.get('db'),'meals',req.params.id,mealToUpdate)
         .then(()=>res.status(200).json('Success'))
         .catch(next)
 
   })
-// commented this out because it won't work in its current state
+
 
 /*
 async function checkMealExists(req, res, next) {
   try {
     const meal = await MealsService.getById(
-   
-
-     
     MealsService.insertMeal(
       req.app.get('db'),
       req.params.meal_id
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -149,8 +149,15 @@ const tools={
             "age": 18, "gender": "Male", "weight": "130", "height":"70"
         }
         const newMeal={
-            "userId": 2,
-            "alldaycalories": 900,
+            "userid": 1, 
+            "dateofmeal": "2020-09-23",
+            "breakfast_food": "milk",
+            "breakfast_calories": 200,
+            "lunch_food": "pasta",
+            "lunch_calories": 300,
+            "dinner_food": "chicken noodle soup",
+            "dinner_calories": 300,
+            "alldaycalories": 800
         }
         return {newUser,newMeal}
     },
@@ -160,7 +167,12 @@ const tools={
             //{"Without_password":{"username": "newUserName"}}
         ]
         const meal=[
-            {"Update_meal":{"alldaycalories":800}}
+            {"Update_meal":{
+                "breakfast_calories": 200,
+                "lunch_calories": 300,
+                "dinner_calories": 300,
+                "alldaycalories": 800
+            }}
         ]
         return {user,meal}
     },
